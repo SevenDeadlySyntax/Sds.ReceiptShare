e0450847ca7c4aa20cfb15d95b1acb9a303d7368
Edward.Earle <edward.earle>
2017-06-25 23:34:27 +0100

    Continued work on Groups put and post methods, including fixes to datacontext and initialiser

--- Sds.ReceiptShare.Api/Controllers/GroupsController.cs
+++ Sds.ReceiptShare.Api/Controllers/GroupsController.cs
@@ -67,34 +67,58 @@ namespace Sds.ReceiptShare.Api.Controllers
         /// <summary>
         /// Adds a new group
         /// TODO - need to identify the user and set them as the owner
-        /// </summary>
-        /// <param name="value"></param>
-        [HttpPost]
+        /// Example request body: {"Id": 0,"Name": "Test Group","AdminId": 1}
+    /// </summary>
+    /// <param name="value"></param>
+    [HttpPost]
         public IActionResult Post([FromBody]Group group)
         {
-            var administrator = _context.Members.First(s => s.Id == group.Id);
+            if (group == null || group.Name == null || group.AdminId == 0) return BadRequest("Required fields not provided.");
+            var administrator = _context.Members.First(s => s.Id == group.AdminId);
             var newGroup = _context.Add(new Domain.Models.Group { Name = group.Name, Created = DateTime.Now, Administrator = administrator });
             _context.SaveChanges();
 
-            return Ok(newGroup)
+            return Ok(newGroup);
         }
-
-        // PUT api/values/5
+        
+        /// <summary>
+        /// Sample body: [2,3]
+        /// Sample URL: api/groups/1/AddMembers
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="ids"></param>
+        /// <returns></returns>
         [HttpPut("{id}/AddMembers")]
         public IActionResult Members(int id, [FromBody]int[] ids)
         {
-            var group = _context.Groups.FirstOrDefault(s => s.Id == id);
-
-            if (group == null) return NoContent();
             var successCount = 0;
 
+            // If no member ids, return bad request
+            if (ids == null || !ids.Any()) return BadRequest("No members specified");
+
+            // If group not found, return No Content (TBC: or not found??)
+            var group = _context.Groups
+                .Include(s => s.Members)
+                .FirstOrDefault(s => s.Id == id);
+            if (group == null) return NoContent();
+                        
+            if (group.Members == null) group.Members = new List<Domain.Models.GroupMember>();
+
+            // For each member specified...
             foreach (var memberId in ids)
             {
-                var member = _context.Members.FirstOrDefault(s => s.Id == memberId);
-                if (member != null)
-                {
-                    group.Members.Add(new Domain.Models.GroupMember { Member = member });
-                    successCount++;
+                // Check that the member does not already exist in the group
+                if (!group.Members.Any(s => s.MemberId == memberId)) {
+
+                    // Get the matching member record
+                    var member = _context.Members.FirstOrDefault(s => s.Id == memberId);
+                    if (member != null)
+                    {
+                        // Create new group member record
+                       // _context.GroupMembers.Add(new Domain.Models.GroupMember { MemberId = memberId });
+                        group.Members.Add(new Domain.Models.GroupMember { MemberId = memberId });
+                        successCount++;
+                    }
                 }
             }
             _context.SaveChanges();
